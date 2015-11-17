f64bc043832488c07f88458ee564e9caa4215bd4
JackNel <jcneligan@gmail.com>
2015-11-17 12:14:14 -0500

    Initial import

---
+++ src/main/java/com/theironyard/configs/WebConfig.java
@@ -0,0 +1,16 @@
+package com.theironyard.configs;
+
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by Jack on 11/17/15.
+ */
+
+// This class serves the public folder
+public class WebConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("public/**:");
+    }
+}
